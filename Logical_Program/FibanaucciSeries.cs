962bf4dbf31236c9ab5f0b276d9c2397b85bf770
Bhagya Raju <bhagyarajusrivatsa12@gmail.com>
2023-02-24 23:45:51 +0530

    LogicalProgram

---
+++ Logical_Program/FibanaucciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Program
+{
+    public class FibanaucciSeries
+
+    {
+        public static void Fibanaucci_Series()
+        {
+            int n = 10; // number of terms to generate
+            int firstTerm = 0, secondTerm = 1, nextTerm;
+
+            Console.WriteLine("Fibonacci Series:");
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(firstTerm + " ");
+
+                nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
--- Logical_Program/Program.cs
+++ Logical_Program/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical Program");
+            //Console.WriteLine("Welcome to Logical Program");
+            FibanaucciSeries.Fibanaucci_Series();
         }
     }
 }
